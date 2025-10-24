9b3cab0bf11de844fd0cf83f83729f1283f507de
Beimni406 <bkifle@my.yearupunited.org>
2025-10-24 11:03:31 -0400

    added constructor for Vehicle class

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,7 +1,6 @@
 package com.pluralsight;
 
 public class Vehicle {
-    // vehicle details
     private int vin;
     private int year;
     private String make;
@@ -10,4 +9,16 @@ public class Vehicle {
     private String color;
     private int odometer;
     private double price;
+
+    // constructor to set values
+    public Vehicle(int vin, int year, String make, String model, String type, String color, int odometer, double price) {
+        this.vin = vin;
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.type = type;
+        this.color = color;
+        this.odometer = odometer;
+        this.price = price;
+    }
 }
