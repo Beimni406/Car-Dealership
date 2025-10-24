9f37cce2b09d7247ea09c760bfb992d86e50cd0d
Beimni406 <bkifle@my.yearupunited.org>
2025-10-24 10:56:07 -0400

    added fields for vehicle info

---
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class Vehicle {
+    // vehicle details
+    private int vin;
+    private int year;
+    private String make;
+    private String model;
+    private String type;
+    private String color;
+    private int odometer;
+    private double price;
+}
