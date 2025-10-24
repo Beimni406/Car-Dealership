adde86c0c88d5eed15a69a45450c08bbd7e4e9c0
Beimni406 <bkifle@my.yearupunited.org>
2025-10-24 11:11:21 -0400

    added getters and setters

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -10,7 +10,6 @@ public class Vehicle {
     private int odometer;
     private double price;
 
-    // constructor to set values
     public Vehicle(int vin, int year, String make, String model, String type, String color, int odometer, double price) {
         this.vin = vin;
         this.year = year;
@@ -21,4 +20,24 @@ public class Vehicle {
         this.odometer = odometer;
         this.price = price;
     }
+
+    // Getters
+    public int getVin() { return vin; }
+    public int getYear() { return year; }
+    public String getMake() { return make; }
+    public String getModel() { return model; }
+    public String getType() { return type; }
+    public String getColor() { return color; }
+    public int getOdometer() { return odometer; }
+    public double getPrice() { return price; }
+
+    // Setters
+    public void setVin(int vin) { this.vin = vin; }
+    public void setYear(int year) { this.year = year; }
+    public void setMake(String make) { this.make = make; }
+    public void setModel(String model) { this.model = model; }
+    public void setType(String type) { this.type = type; }
+    public void setColor(String color) { this.color = color; }
+    public void setOdometer(int odometer) { this.odometer = odometer; }
+    public void setPrice(double price) { this.price = price; }
 }
