6677ea9a8095893e5fd891114fe6422b2c001181
Beimni406 <bkifle@my.yearupunited.org>
2025-10-28 13:18:03 -0400

    Dealership class with fields and added constructor

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -2,9 +2,18 @@ package com.pluralsight;
 
 import java.util.ArrayList;
 
+// This class represents a car dealership and stores a list of vehicles
 public class Dealership {
     private String name;
     private String address;
     private String phone;
     private ArrayList<Vehicle> inventory;
+
+    // Constructor for Dealership
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>(); // start with an empty list
+    }
 }
